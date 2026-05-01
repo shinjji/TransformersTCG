886580b462ddeaeaad9b91d8746477482b58dee0
Mark <markmckay001@gmail.com>
2026-05-01 17:52:48 +0100

    rules data

---
+++ data/rules.js
@@ -0,0 +1,77 @@
+const RULES_DATA = [
+  {
+    term: 'Bandit',
+    suffix: '',
+    definition: 'Blue battle icons are ignored and have no effect when this character is defending'
+  },
+  {
+    term: 'Bold',
+    suffix: 'X',
+    definition: 'Flip X additional battle cards when this character is attacking'
+  },
+  {
+    term: 'Bounty',
+    suffix: '',
+    definition: 'The Bounty reward is triggered when this attacking character KOs an enemy'
+  },
+  {
+    term: 'Brave',
+    suffix: '',
+    definition: 'The attacker must attack an eligible target with Brave over any other eligible targets'
+  },
+  {
+    term: 'Drifter',
+    suffix: '',
+    definition: 'Orange battle icons are ignored and have no effect when this character is attacking'
+  },
+  {
+    term: 'Focus',
+    suffix: 'X',
+    definition: 'Flip X battle cards before a battle begins. You may scrap any and put any back on top in any order'
+  },
+  {
+    term: 'Horde',
+    suffix: 'X',
+    definition: 'Your starting team may have up to X characters with this name'
+  },
+  {
+    term: 'Oppress',
+    suffix: 'X',
+    definition: 'All characters in a battle must have their Bold and Tough totals reduced by X'
+  },
+  {
+    term: 'Pierce',
+    suffix: 'X',
+    definition: 'Dp at least X damage when attacking (but not more than the attack total)'
+  },
+  {
+    term: 'Plan',
+    suffix: 'X',
+    definition: 'Before a battle begins, put up to X cards from your hand on top of your deck in any order'
+  },
+  // {
+  //   term: 'Recon',
+  //   suffix: '',
+  //   definition: 'This character may attack characters with Stealth.'
+  // },
+  {
+    term: 'Revenge',
+    suffix: '',
+    definition: 'The Revenge reward is triggered when this character is KOd'
+  },
+  {
+    term: 'Safeguard',
+    suffix: 'X',
+    definition: 'An undamaged character cannot take more than X damage'
+  },
+  {
+    term: 'Stealth',
+    suffix: '',
+    definition: 'This character cannot be targetted by an attack if there are any other eligible targets'
+  },
+  {
+    term: 'Tough',
+    suffix: 'X',
+    definition: 'Flip X additional battle cards when this character is defending'
+  },
+];
